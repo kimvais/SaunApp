ade913edafe7ef68040e68ca66bedf7aa11a533c
Kimmo Parviainen-Jalanko <k@77.fi>
2020-05-31 21:48:24 +0300

    Add whole directory.

---
+++ SaunApp.WPF/App.fs
@@ -0,0 +1,28 @@
+﻿namespace SaunApp.WPF
+
+open System
+
+open System.ComponentModel.Design
+open System.Windows
+open Xamarin.Forms
+open Xamarin.Forms.Platform.WPF
+
+type MainWindow() = 
+    inherit FormsApplicationPage()
+
+module Main = 
+    [<EntryPoint>]
+    [<STAThread>]
+    let main(_args) =
+
+        let app = new System.Windows.Application()
+        Forms.Init()
+        let window = MainWindow()
+        window.Width <- 300.
+        window.Height <- 300.
+        window.WindowStyle <- WindowStyle.ThreeDBorderWindow
+        window.Title <- "SaunaApp"
+        
+        window.LoadApplication(new SaunApp.App())
+
+        app.Run(window)
